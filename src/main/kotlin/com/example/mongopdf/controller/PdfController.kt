87d4a53d0a0f091af4d5bc752fd8febcc619850b
Aphisit Namracha <aphisitnamracha@Aphisits-MacBook-Pro.local>
2020-02-20 10:57:32 +0700

    Add upload multi-pdf

--- src/main/kotlin/com/example/mongopdf/controller/PdfController.kt
+++ src/main/kotlin/com/example/mongopdf/controller/PdfController.kt
@@ -1,16 +1,21 @@
 package com.example.mongopdf.controller
 
 import com.example.mongopdf.service.PdfFileService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
+import kotlin.Exception
 
 @Controller
 @RequestMapping("/pdf")
 class PdfController {
+
+    var logger : Logger = LoggerFactory.getLogger(this.javaClass)
 
     @Autowired
     private lateinit var pdfFileService: PdfFileService
@@ -40,5 +45,35 @@ class PdfController {
         model.addAttribute("title",photo.title)
         model.addAttribute("pdfFile", Base64.getEncoder().encodeToString(photo.file?.data))
         return "pdf"
+    }
+
+    @PostMapping("/add10k")
+    fun addPhoto10k(@RequestParam("title") title: String,
+                 @RequestParam("pdf") file: MultipartFile, model: Model) : String {
+        var id : String? = null
+        try {
+            for (i in 1..10000) {
+                logger.info("file index $i")
+                id = pdfFileService.addPdfFile(title, file)
+            }
+        }catch (e: Exception){
+            logger.error("$e")
+        }
+        return "redirect:/pdf/$id"
+    }
+
+    @PostMapping("/add100k")
+    fun addPhoto100k(@RequestParam("title") title: String,
+                 @RequestParam("pdf") file: MultipartFile, model: Model) : String {
+        var id : String? = null
+        try {
+            for (i in 1..100000) {
+                logger.info("file index $i")
+                id = pdfFileService.addPdfFile(title, file)
+            }
+        }catch (e: Exception){
+            logger.error("$e")
+        }
+        return "redirect:/pdf/$id"
     }
 }
